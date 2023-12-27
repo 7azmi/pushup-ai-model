805e9c01631adc9271303296bfa09abc87a71efa
7azmi <a7.h3.04@gmail.com>
2023-12-28 02:32:31 +0800

    Ink added

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 import uvicorn
@@ -7,7 +8,11 @@ from fastapi import FastAPI, Request, HTTPException, Form
 import pushup
 
 app = FastAPI()
-KEY = os.environ.get('AI_API_KEY', b'OR9Hdu3NKcaT4PPHJni3NAepp61DL_SGeOmB2Eg7PT0=')
+KEY = os.environ.get('AI_API_KEY', 'OR9Hdu3NKcaT4PPHJni3NAepp61DL_SGeOmB2Eg7PT0=')
+test = os.environ.get('TST', 'it is not working')
+print(test)
+
+
 
 
 @app.get("/")
@@ -22,10 +27,16 @@ def test():
 
 
 def decrypt_message(encrypted_message):
+    #key = KEY.encode()
     f = Fernet(KEY)
     decrypted_message = f.decrypt(encrypted_message).decode()
     return decrypted_message
 
+def encrypt_message(message):
+    #key = KEY.encode()
+    f = Fernet(KEY)
+    encrypted_message = f.encrypt(message.encode())
+    return encrypted_message
 
 @app.post('/count_pushups')
 async def count_pushups_route(encrypted_video_url: str = Form(...)):
@@ -39,4 +50,8 @@ async def count_pushups_route(encrypted_video_url: str = Form(...)):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    lol = 'hehe boi'
+
+    #print(decrypt_message(encrypt_message(lol)))
+
     uvicorn.run(app, port=5000, host="0.0.0.0")
